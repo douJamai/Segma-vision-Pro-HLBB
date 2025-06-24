0d34397844a09c41b6a7ad8ddfb894b8acb3f70d
Douae Jamai <douaejamai03@gmail.com>
2025-06-24 15:07:00 +0200

    Fix Read the Docs build configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,37 +1,22 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'Segma Vision Pro'
 copyright = '2025, DOUAE JAMAI'
 author = 'DOUAE JAMAI'
 release = '1.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = [
-    'sphinx.ext.autodoc',  # Ajouté pour la documentation automatique
-    'sphinx.ext.viewcode', # Ajouté pour voir le code source
-]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-language = 'en'  # Changé de 'english' à 'en'
+language = 'en'
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-# Ajout optionnel pour éviter les erreurs si _static n'existe pas
-import os
-if not os.path.exists('_static'):
-    html_static_path = []
-
+# Configuration pour éviter les erreurs
+master_doc = 'index'
